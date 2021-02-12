550be1fae6653a05f6710bb63bdbda08d2bc433e
Lexy Kramer <lexy.kramer@gmail.com>
2021-02-12 13:12:39 -0500

    Connected to remote db

--- server/database/queryMethods.js
+++ server/database/queryMethods.js
@@ -1,11 +1,11 @@
 const { MongoClient } = require('mongodb');
 
-var url = 'mongodb://localhost:27017';
+var url = 'mongodb://lexy:shinjiro888@3.85.164.109:27017/scarebnb';
 const getLodging = function (id, cb) {
   MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
     if (err) throw err;
-    var dbo = db.db('scareBnb');
-    dbo.collection('lodgings').find({ _id: id }).toArray((err, result) => {
+    var dbo = db.db('scarebnb');
+    dbo.collection('lodging').find({ _id: id }).toArray((err, result) => {
       if (err) throw err;
       getHost(result[0].host, cb, result)
       db.close();
@@ -16,7 +16,7 @@ const getLodging = function (id, cb) {
 const getHost = function (id, cb, result) {
   MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
     if (err) throw err;
-    var dbo = db.db('scareBnb');
+    var dbo = db.db('scarebnb');
     dbo.collection('hosts').find({ _id: id }, { _id: 0}).toArray((err, hostInfo) => {
       if (err) throw err;
       result[0].host = hostInfo[0];
@@ -29,7 +29,7 @@ const getHost = function (id, cb, result) {
 const reserveLoge = function (data) {
   MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
     if (err) throw err;
-    var dbo = db.db('scareBnb');
+    var dbo = db.db('scarebnb');
     dbo.collection('reservations').insertOne(data)
       .then(() => {
         console.log('success!!!!!!');
